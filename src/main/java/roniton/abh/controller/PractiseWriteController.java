77e79bbf9d0efdb1f605d6e621e2153c41ab0ee1
Roniton <roniton@gmx.de>
2024-09-18 23:19:44 +0200

    started practise writing

--- src/main/java/roniton/abh/controller/PractiseWriteController.java
+++ src/main/java/roniton/abh/controller/PractiseWriteController.java
@@ -1,7 +1,12 @@
 package roniton.abh.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import org.fulib.fx.annotation.controller.Controller;
 import org.fulib.fx.annotation.controller.Title;
+import org.fulib.fx.annotation.event.OnRender;
+import roniton.abh.App;
 
 import javax.inject.Inject;
 
@@ -9,6 +14,22 @@ import javax.inject.Inject;
 @Title("AurebeshHelper - Practise Writing")
 public class PractiseWriteController {
     @Inject
+    App app;
+    @FXML
+    TextField toTranslateBox;
+    @FXML
+    TextField inputBox;
+
+    @Inject
     public PractiseWriteController() {
+    }
+
+    @OnRender
+    void render(){
+        toTranslateBox.setText("a");
+    }
+
+    public void onReturn() {
+        app.show("/menu");
     }
 }
