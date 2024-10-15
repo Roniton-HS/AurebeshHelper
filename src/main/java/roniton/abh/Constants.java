b1062d6a33ebf225931a74dcf4a626de89eab002
Roniton <roniton@gmx.de>
2024-10-15 18:40:07 +0200

    bugfix: W in obi-wan was capital and therefore couldn't be solved

--- src/main/java/roniton/abh/Constants.java
+++ src/main/java/roniton/abh/Constants.java
@@ -3,7 +3,7 @@ package roniton.abh;
 import java.util.List;
 
 public class Constants {
-    public static final List<String> words = List.of("hello there", "sheev palpatine", "obi-Wan kenobi", "lightsaber", "force",
+    public static final List<String> words = List.of("hello there", "sheev palpatine", "obi-wan kenobi", "lightsaber", "force",
             "han solo", "skywalker", "yoda", "x-wing", "high ground", "darth vader", "darth maul", "naboo", "coruscant",
             "alderaan", "death star", "han shot first", "jabba", "mustafar", "mandalorian", "grogu", "captain rex",
             "commander cody", "general grievous", "battle droid", "i am the senate", "unlimited power", "hyper drive",
